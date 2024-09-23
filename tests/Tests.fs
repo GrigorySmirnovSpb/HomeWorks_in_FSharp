f82659f9590f861d28b80320e80d3ad97cc2d3fc
Grirory_Smirnov <smirnov2006.76@mail.ru>
2024-09-23 22:55:48 +0300

    Initial BubbleSort

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -1,14 +1,31 @@
-namespace FacTest
+namespace tests
 
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
-open FactorLib
 
 [<TestClass>]
 type TestClass() =
 
     [<TestMethod>]
-    member this.TestMethodPassing() =
-        let expected = 120
-        let actual = FactorLib.Say.factor 5
-        Assert.AreEqual(expected, actual)
+    member this.TestMethodPassing1() =
+        let expected: int[] = [| 1; 2; 3; 4; 5; 6; 7; 8 |]
+        let actual: int[] = BubbleLib.Say.Bubblesort [| 3; 4; 1; 8; 5; 2; 7; 6 |]
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing2() =
+        let expected: int[] = [| -12; -4; 0; 4; 7; 10 |]
+        let actual: int[] = BubbleLib.Say.Bubblesort [| 0; 10; -4; 4; 7; -12 |]
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing3() =
+        let expected: int[] = [| 1 |]
+        let actual: int[] = BubbleLib.Say.Bubblesort [| 1 |]
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing4() =
+        let expected: int[] = [| -10; -10; -6; 0; 0 |]
+        let actual: int[] = BubbleLib.Say.Bubblesort [| 0; -10; -6; 0; -10 |]
+        CollectionAssert.AreEqual(expected, actual)
