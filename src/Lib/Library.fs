12f58c48461127b716961b58f972af2b012530da
Grirory_Smirnov <smirnov2006.76@mail.ru>
2024-09-23 22:58:28 +0300

    Initial Fib

--- src/Lib/Library.fs
+++ src/Lib/Library.fs
@@ -1,25 +1,45 @@
-﻿namespace BubbleLib
+﻿namespace Fib
 
 open System
 
 module Say =
-    let mutable arr1: int array = Array.zeroCreate 5
+    type Matrix = int array array
 
-    let Bubblesort arr1 =
-        let mutable f: int = 1
-        let mutable i: int = 0
+    let q: Matrix = [| [| 1; 1 |]; [| 1; 0 |] |]
 
-        while (f = 1) do
-            let j: int = 0
-            f <- 0
+    let createMatrixArray (size: int) (rows: int) (cols: int) : Matrix array =
+        Array.init size (fun _ -> Array.init rows (fun _ -> Array.init cols (fun _ -> 0)))
 
-            for j in 0 .. Array.length arr1 - 2 - i do
-                if (arr1[j] > arr1[j + 1]) then
-                    arr1[j] <- arr1[j] ^^^ arr1[j + 1]
-                    arr1[j + 1] <- arr1[j] ^^^ arr1[j + 1]
-                    arr1[j] <- arr1[j] ^^^ arr1[j + 1]
-                    f <- 1
+    let multiplyMatrix (mat1: Matrix) (mat2: Matrix) : Matrix =
+        let res = Array.init 2 (fun _ -> Array.create 2 0)
 
-            i <- i + 1
+        for i in 0..1 do
+            for j in 0..1 do
+                for k in 0..1 do
+                    res.[i].[j] <- res.[i].[j] + mat1.[i].[k] * mat2.[k].[j]
 
-        arr1
+        res
+
+    let mutable ArrMatr = createMatrixArray 100 2 2
+    ArrMatr.[1] <- [| [| 1; 1 |]; [| 1; 0 |] |]
+
+    let rec powerMatrix (mat: Matrix) (p: int) : Matrix =
+        if p = 1 then
+            mat
+        else if ArrMatr.[p].[0].[0] <> 0 then
+            ArrMatr.[p]
+        else
+            let m1 = powerMatrix mat (p / 2)
+            let m2 = powerMatrix mat (p / 2 + p % 2)
+            let res = multiplyMatrix m1 m2
+            ArrMatr.[p] <- res
+            res
+
+    let getnumber (n: int) : int =
+        if n = 0 then
+            0
+        else if n = 1 then
+            1
+        else
+            let resmat = powerMatrix q (n - 1)
+            resmat.[0].[0]
--- tests/Tests.fs
+++ tests/Tests.fs
@@ -7,25 +7,7 @@ open Microsoft.VisualStudio.TestTools.UnitTesting
 type TestClass() =
 
     [<TestMethod>]
-    member this.TestMethodPassing1() =
-        let expected: int[] = [| 1; 2; 3; 4; 5; 6; 7; 8 |]
-        let actual: int[] = BubbleLib.Say.Bubblesort [| 3; 4; 1; 8; 5; 2; 7; 6 |]
-        CollectionAssert.AreEqual(expected, actual)
-
-    [<TestMethod>]
-    member this.TestMethodPassing2() =
-        let expected: int[] = [| -12; -4; 0; 4; 7; 10 |]
-        let actual: int[] = BubbleLib.Say.Bubblesort [| 0; 10; -4; 4; 7; -12 |]
-        CollectionAssert.AreEqual(expected, actual)
-
-    [<TestMethod>]
-    member this.TestMethodPassing3() =
-        let expected: int[] = [| 1 |]
-        let actual: int[] = BubbleLib.Say.Bubblesort [| 1 |]
-        CollectionAssert.AreEqual(expected, actual)
-
-    [<TestMethod>]
-    member this.TestMethodPassing4() =
-        let expected: int[] = [| -10; -10; -6; 0; 0 |]
-        let actual: int[] = BubbleLib.Say.Bubblesort [| 0; -10; -6; 0; -10 |]
-        CollectionAssert.AreEqual(expected, actual)
+    member this.TestMethodPassing() =
+        let expected = 21
+        let actual = Fib.Say.getnumber 8
+        Assert.AreEqual(expected, actual)
