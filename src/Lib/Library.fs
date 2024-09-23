f10580ca3cdde4bbd56441c63988530366c44581
Grirory_Smirnov <smirnov2006.76@mail.ru>
2024-09-23 22:58:28 +0300

    Update all

--- src/Lib/Library.fs
+++ src/Lib/Library.fs
@@ -1,5 +1,48 @@
-﻿namespace Lib
+﻿namespace FibLib
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+
+module Fib =
+    type Matrix = int array array
+
+    let q: Matrix = [| [| 1; 1 |]; [| 1; 0 |] |]
+
+    let createMatrixArray (size: int) (rows: int) (cols: int) : Matrix array =
+       Array.init size (fun _ -> Array.init rows (fun _ -> Array.init cols (fun _ -> 0)))
+
+    let multiplyMatrix (mat1: Matrix) (mat2: Matrix) : Matrix =
+        let res = Array.init 2 (fun _ -> Array.create 2 0)
+
+        for i in 0..1 do
+            for j in 0..1 do
+                for k in 0..1 do
+                    res.[i].[j] <- res.[i].[j] + mat1.[i].[k] * mat2.[k].[j]
+
+        res
+
+
+    let rec powerMatrix (ArrRef: ref<array<Matrix>>) (mat: Matrix) (p: int) : Matrix =
+        if p = 1 then
+            mat
+        else if ArrRef.Value.[p].[0].[0] <> 0 then
+            ArrRef.Value.[p]
+        else
+            let m1 = powerMatrix ArrRef mat (p / 2)
+            let m2 = powerMatrix ArrRef mat (p / 2 + p % 2)
+            let res = multiplyMatrix m1 m2
+            ArrRef.Value.[p] <- res
+            res
+
+    let getnumber (n: int) : int =
+        if n = 0 then
+            0
+        else if n = 1 then
+            1
+        else if n < 0 then
+            0
+        else
+            let mutable ArrMatr = createMatrixArray (n) 2 2
+            let mRef = ref ArrMatr
+            ArrMatr.[1] <- [| [| 1; 1 |]; [| 1; 0 |] |]
+            let resmat = powerMatrix mRef q (n-1)
+            resmat.[0].[0]
--- tests/Tests.fs
+++ tests/Tests.fs
@@ -4,8 +4,41 @@ open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
 [<TestClass>]
-type TestClass () =
+type TestClass() =
 
     [<TestMethod>]
-    member this.TestMethodPassing () =
-        Assert.IsTrue(true);
+    member this.TestMethodPassing1() =
+        let expected = 0
+        let actual = FibLib.Fib.getnumber 0
+        Assert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing2() =
+        let expected = 1
+        let actual = FibLib.Fib.getnumber 1
+        Assert.AreEqual(expected, actual)
+
+
+    [<TestMethod>]
+    member this.TestMethodPassing3() =
+        let expected = 1
+        let actual = FibLib.Fib.getnumber 2
+        Assert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing6() =
+        let expected = 4181
+        let actual = FibLib.Fib.getnumber 19
+        Assert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing4() =
+        let expected = 121393
+        let actual = FibLib.Fib.getnumber 26
+        Assert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing5() =
+        let expected = 0
+        let actual = FibLib.Fib.getnumber -1
+        Assert.AreEqual(expected, actual)
