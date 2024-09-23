f61b3fa669e99447021831c9be9a144640af8ea3
Grirory_Smirnov <smirnov2006.76@mail.ru>
2024-09-23 22:55:48 +0300

    Fix conflicts and delete diffs

--- src/Lib/Library.fs
+++ src/Lib/Library.fs
@@ -1,5 +1,25 @@
-﻿namespace Lib
+﻿namespace BubbleLib
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+
+module Bubblesort =
+    let mutable arr1: int array = [||]
+
+    let Bubblesort arr1 =
+        let mutable f: int = 1
+        let mutable i: int = 0
+
+        while (f = 1) do
+            let j: int = 0
+            f <- 0
+
+            for j in 0 .. Array.length arr1 - 2 - i do
+                if (arr1[j] > arr1[j + 1]) then
+                    let temp = arr1.[j]
+                    arr1.[j] <- arr1.[j+1]
+                    arr1.[j+1] <- temp
+                    f <- 1
+
+            i <- i + 1
+
+        arr1
--- tests/Tests.fs
+++ tests/Tests.fs
@@ -4,8 +4,46 @@ open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
 [<TestClass>]
-type TestClass () =
+type TestClass() =
 
     [<TestMethod>]
-    member this.TestMethodPassing () =
-        Assert.IsTrue(true);
+    member this.TestMethodPassing1() =
+        let expected: int[] = [| 1; 2; 3; 4; 5; 6; 7; 8 |]
+        let actual: int[] = BubbleLib.Bubblesort.Bubblesort [| 3; 4; 1; 8; 5; 2; 7; 6 |]
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing2() =
+        let expected: int[] = [| -12; -4; 0; 4; 7; 10 |]
+        let actual: int[] = BubbleLib.Bubblesort.Bubblesort [| 0; 10; -4; 4; 7; -12 |]
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing3() =
+        let expected: int[] = [| 1 |]
+        let actual: int[] = BubbleLib.Bubblesort.Bubblesort [| 1 |]
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing4() =
+        let expected: int[] = [| -10; -10; -6; 0; 0 |]
+        let actual: int[] = BubbleLib.Bubblesort.Bubblesort [| 0; -10; -6; 0; -10 |]
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing5() =
+        let expected: int[] = [||]
+        let actual: int[] = BubbleLib.Bubblesort.Bubblesort [||]
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing6() =
+        let expected: int[] = [| 1; 2; 3; 4; 5; 6; 7; 8 |]
+        let actual: int[] = BubbleLib.Bubblesort.Bubblesort [| 1; 2; 3; 4; 5; 6; 7; 8 |]
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing7() =
+        let expected: int[] = [| 1; 2; 3; 4; 5; 6; 7; 8 |]
+        let actual: int[] = BubbleLib.Bubblesort.Bubblesort [| 8; 7; 6; 5; 4; 3; 2; 1|]
+        CollectionAssert.AreEqual(expected, actual)
