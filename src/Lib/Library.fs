e79ceb724275b8abfeb1e085a4918fd4afd71621
Grirory_Smirnov <smirnov2006.76@mail.ru>
2024-09-30 17:56:30 +0300

    Create MergeSort

--- src/Lib/Library.fs
+++ src/Lib/Library.fs
@@ -1,5 +1,26 @@
-﻿namespace Lib
+﻿namespace MergeSort
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module MergeSort =
+
+    let rec MergeSort (arr: 'a array) (compare):'a array = 
+        if Array.length arr = 1
+        then
+            arr
+        else 
+            let arr1 = MergeSort (Array.sub arr 0 ((Array.length arr)/2)) compare
+            let arr2 = MergeSort (Array.sub arr  (Array.length arr/2) ((Array.length arr)/2 + (Array.length arr)%2)) compare
+            let mutable arr3: 'a array =[||]
+            let mutable j: int = 0
+            let mutable i: int = 0
+            while (i < Array.length arr1) && (j < Array.length arr2) do
+                if compare arr1.[i] arr2.[j] < 0
+                then
+                    arr3 <- Array.concat [arr3; [|arr1.[i]|]]
+                    i <- i + 1
+                else 
+                    arr3 <- Array.concat [arr3; [|arr2.[j]|]]
+                    j <- j + 1
+            if i >= Array.length arr1
+            then arr3 <- Array.concat [arr3; Array.sub arr2 j (Array.length arr2 - j)]
+            else arr3 <- Array.concat [arr3; Array.sub arr1 i (Array.length arr1 - i)]
+            arr3
--- tests/Tests.fs
+++ tests/Tests.fs
@@ -7,5 +7,13 @@ open Microsoft.VisualStudio.TestTools.UnitTesting
 type TestClass () =
 
     [<TestMethod>]
-    member this.TestMethodPassing () =
-        Assert.IsTrue(true);
+    member this.TestMethodPassing1 () =
+        let expected = [|1; 2; 3; 4; 5; 6; 7|]
+        let actual = MergeSort.MergeSort.MergeSort [|5; 1; 6; 4; 3; 7; 2|] compare
+        CollectionAssert.AreEqual(expected,  actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing2 () =
+        let expected = [|"abcd"; "abcde"; "abcdef"|]
+        let actual = MergeSort.MergeSort.MergeSort [|"abcde"; "abcd"; "abcdef"|] compare
+        CollectionAssert.AreEqual(expected,  actual)
