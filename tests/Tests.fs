393596029509f04f68b933204b3f3568abbdb3c4
Grirory_Smirnov <smirnov2006.76@mail.ru>
2024-10-05 11:00:05 +0300

    add Fantomas

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -1,11 +1,10 @@
-namespace tests
+﻿namespace tests
 
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
 [<TestClass>]
-type TestClass () =
+type TestClass() =
 
     [<TestMethod>]
-    member this.TestMethodPassing () =
-        Assert.IsTrue(true);
+    member this.TestMethodPassing() = Assert.IsTrue(true)
