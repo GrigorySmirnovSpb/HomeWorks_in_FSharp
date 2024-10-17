d1efeed0bbf24074e73694ec5c7f667ff4842e7e
Grirory_Smirnov <smirnov2006.76@mail.ru>
2024-10-17 17:01:45 +0300

    Create

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -7,4 +7,9 @@ open Microsoft.VisualStudio.TestTools.UnitTesting
 type TestClass() =
 
     [<TestMethod>]
-    member this.TestMethodPassing() = Assert.IsTrue(true)
+    member this.Test1() =
+        let expected = [ 1; 2; 3; 4; 5 ]
+        let mutable lst = Lists.MyList.fromList [ 4; 5; 2; 1; 3 ]
+        let lst2 = lst.MergeSort compare
+        let actual = Lists.MyList.toList lst2
+        Assert.AreEqual(expected, actual)
