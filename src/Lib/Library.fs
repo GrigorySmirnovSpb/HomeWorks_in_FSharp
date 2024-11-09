ba1061453315d9d5b46dc14617b9af584ee2b2f3
Grirory_Smirnov <smirnov2006.76@mail.ru>
2024-11-09 20:55:14 +0300

    Update Library.fs and add new tests

--- src/Lib/Library.fs
+++ src/Lib/Library.fs
@@ -1,30 +1,34 @@
 ﻿namespace QuickSort
 
 module Quicksort =
-    let rec QuickSort (arr1: int array) (l: int) (r: int) : int array =
-        let mutable arr: int array = arr1
-        let pivot: int = arr.[(l + r) / 2]
-        let mutable i: int = l
-        let mutable j: int = r
+    let QuickSort (arr1: int array) : int array =
+        let rec QuickHelper (arr1: int array) (l: int) (r: int) =
+            let mutable arr: int array = arr1
+            let pivot: int = arr.[(l + r) / 2]
+            let mutable i: int = l
+            let mutable j: int = r
 
-        while i <= j do
-            while arr.[i] < pivot do
-                i <- i + 1
+            while i <= j do
+                while arr.[i] < pivot do
+                    i <- i + 1
 
-            while arr.[j] > pivot do
-                j <- j - 1
+                while arr.[j] > pivot do
+                    j <- j - 1
 
-            if i <= j then
-                let c = arr.[i]
-                arr.[i] <- arr.[j]
-                arr.[j] <- c
-                i <- i + 1
-                j <- j - 1
+                if i <= j then
+                    let c = arr.[i]
+                    arr.[i] <- arr.[j]
+                    arr.[j] <- c
+                    i <- i + 1
+                    j <- j - 1
 
-        if l < j then
-            arr <- QuickSort arr l j
+            if l < j then
+                arr <- QuickHelper arr l j
 
-        if r > i then
-            arr <- QuickSort arr i r
+            if r > i then
+                arr <- QuickHelper arr i r
 
-        arr
+            arr
+        if arr1.Length > 0
+        then QuickHelper arr1 0 (arr1.Length-1)
+        else [||]
--- tests/Tests.fs
+++ tests/Tests.fs
@@ -9,23 +9,35 @@ type TestClass() =
     [<TestMethod>]
     member this.TestMethodPassing1() =
         let expected: int[] = [| 1; 2; 3; 4; 5; 6; 7; 8 |]
-        let actual: int[] = QuickSort.Quicksort.QuickSort [| 3; 4; 1; 8; 5; 2; 7; 6 |] 0 7
+        let actual: int[] = QuickSort.Quicksort.QuickSort [| 3; 4; 1; 8; 5; 2; 7; 6 |]
         CollectionAssert.AreEqual(expected, actual)
 
     [<TestMethod>]
     member this.TestMethodPassing2() =
         let expected: int[] = [| -12; -4; 0; 4; 7; 10 |]
-        let actual: int[] = QuickSort.Quicksort.QuickSort [| 0; 10; -4; 4; 7; -12 |] 0 5
+        let actual: int[] = QuickSort.Quicksort.QuickSort [| 0; 10; -4; 4; 7; -12 |]
         CollectionAssert.AreEqual(expected, actual)
 
     [<TestMethod>]
     member this.TestMethodPassing3() =
         let expected: int[] = [| 1 |]
-        let actual: int[] = QuickSort.Quicksort.QuickSort [| 1 |] 0 0
+        let actual: int[] = QuickSort.Quicksort.QuickSort [| 1 |]
         CollectionAssert.AreEqual(expected, actual)
 
     [<TestMethod>]
     member this.TestMethodPassing4() =
         let expected: int[] = [| -10; -10; -6; 0; 0 |]
-        let actual: int[] = QuickSort.Quicksort.QuickSort [| 0; -10; -6; 0; -10 |] 0 4
+        let actual: int[] = QuickSort.Quicksort.QuickSort [| 0; -10; -6; 0; -10 |]
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing5() =
+        let expected: int[] = [||]
+        let actual: int[] = QuickSort.Quicksort.QuickSort [||]
+        CollectionAssert.AreEqual(expected, actual)
+    
+    [<TestMethod>]
+    member this.TestMethodPassing6() =
+        let expected: int[] = [| 1; 2; 3; 4; 5; 6; 7; 8 |]
+        let actual: int[] = QuickSort.Quicksort.QuickSort [| 1; 2; 3; 4; 5; 6; 7; 8 |]
         CollectionAssert.AreEqual(expected, actual)
