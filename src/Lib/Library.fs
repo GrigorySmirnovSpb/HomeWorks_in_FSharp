305e703e7a5b17b19a4af3417aa8f866dd5872a6
Grirory_Smirnov <smirnov2006.76@mail.ru>
2024-11-20 11:35:36 +0300

    Rewrite Mergesort

--- src/Lib/Library.fs
+++ src/Lib/Library.fs
@@ -1,26 +1,35 @@
-﻿namespace MergeSort
+﻿namespace LibMergeSort
 
-module MergeSort =
+module MergeSorting =
 
-    let rec MergeSort (arr: 'a array) (compare):'a array = 
-        if Array.length arr = 1
+    let rec MergeSort (arr: 'a array) compare :'a array =
+        if Array.length arr <= 1
         then
             arr
-        else 
+        else
             let arr1 = MergeSort (Array.sub arr 0 ((Array.length arr)/2)) compare
             let arr2 = MergeSort (Array.sub arr  (Array.length arr/2) ((Array.length arr)/2 + (Array.length arr)%2)) compare
-            let mutable arr3: 'a array =[||]
+            let mutable arr3: 'a array = arr
             let mutable j: int = 0
             let mutable i: int = 0
+            let mutable k: int = 0
             while (i < Array.length arr1) && (j < Array.length arr2) do
                 if compare arr1.[i] arr2.[j] < 0
                 then
-                    arr3 <- Array.concat [arr3; [|arr1.[i]|]]
+                    arr3.[k] <- arr1.[i]
                     i <- i + 1
-                else 
-                    arr3 <- Array.concat [arr3; [|arr2.[j]|]]
+                    k <- k + 1
+                else
+                    arr3.[k] <- arr2.[j]
                     j <- j + 1
+                    k <- k + 1
             if i >= Array.length arr1
-            then arr3 <- Array.concat [arr3; Array.sub arr2 j (Array.length arr2 - j)]
-            else arr3 <- Array.concat [arr3; Array.sub arr1 i (Array.length arr1 - i)]
+            then
+                for l in j..(Array.length arr2 - 1) do
+                    arr3.[k] <- arr2.[l]
+                    k <- k + 1
+            else
+                for l in i..(Array.length arr1 - 1) do
+                    arr3.[k] <- arr1.[l]
+                    k <- k + 1
             arr3
--- tests/Tests.fs
+++ tests/Tests.fs
@@ -2,6 +2,7 @@ namespace tests
 
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
+open LibMergeSort
 
 [<TestClass>]
 type TestClass () =
@@ -9,11 +10,41 @@ type TestClass () =
     [<TestMethod>]
     member this.TestMethodPassing1 () =
         let expected = [|1; 2; 3; 4; 5; 6; 7|]
-        let actual = MergeSort.MergeSort.MergeSort [|5; 1; 6; 4; 3; 7; 2|] compare
+        let actual = MergeSorting.MergeSort [|5; 1; 6; 4; 3; 7; 2|] compare
         CollectionAssert.AreEqual(expected,  actual)
 
     [<TestMethod>]
     member this.TestMethodPassing2 () =
         let expected = [|"abcd"; "abcde"; "abcdef"|]
-        let actual = MergeSort.MergeSort.MergeSort [|"abcde"; "abcd"; "abcdef"|] compare
+        let actual = MergeSorting.MergeSort [|"abcde"; "abcd"; "abcdef"|] compare
         CollectionAssert.AreEqual(expected,  actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing3() =
+        let expected = [| 1; 2; 3; 4; 5; 6; 7; 8 |]
+        let actual = MergeSorting.MergeSort[|3; 4; 1; 8; 5; 2; 7; 6 |] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing4() =
+        let expected = [| -12.7; -4.4; 0; 4.2; 7; 10 |]
+        let actual:float array = MergeSorting.MergeSort [| 0; 10; -4.4; 4.2; 7; -12.7|] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing5() =
+        let expected = [| 1 |]
+        let actual = MergeSorting.MergeSort [| 1 |] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing6() =
+        let expected = [| "a"; "b"; "d"; "f"; "g"; "y"; |]
+        let actual = MergeSorting.MergeSort [| "g"; "a"; "f"; "d"; "b"; "y"; |] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestMethodPassing7() =
+        let expected = [||]
+        let actual = MergeSorting.MergeSort [||] compare
+        CollectionAssert.AreEqual(expected, actual)
