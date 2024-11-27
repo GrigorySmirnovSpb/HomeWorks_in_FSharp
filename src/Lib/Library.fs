c9ae39f3419824e3e9553578cc3a63ce09be33cf
Grirory_Smirnov <smirnov2006.76@mail.ru>
2024-11-27 12:02:05 +0300

    Add BubbleSort and QuickSort

--- src/Lib/Library.fs
+++ src/Lib/Library.fs
@@ -2,20 +2,24 @@
 
 module MergeSorting =
 
-    let rec MergeSort (arr: 'a array) compare :'a array =
-        if Array.length arr <= 1
-        then
+    let rec MergeSort (arr: 'a array) compare : 'a array =
+        if Array.length arr <= 1 then
             arr
         else
-            let arr1 = MergeSort (Array.sub arr 0 ((Array.length arr)/2)) compare
-            let arr2 = MergeSort (Array.sub arr  (Array.length arr/2) ((Array.length arr)/2 + (Array.length arr)%2)) compare
+            let arr1 = MergeSort (Array.sub arr 0 ((Array.length arr) / 2)) compare
+
+            let arr2 =
+                MergeSort
+                    (Array.sub arr (Array.length arr / 2) ((Array.length arr) / 2 + (Array.length arr) % 2))
+                    compare
+
             let mutable arr3: 'a array = arr
             let mutable j: int = 0
             let mutable i: int = 0
             let mutable k: int = 0
+
             while (i < Array.length arr1) && (j < Array.length arr2) do
-                if compare arr1.[i] arr2.[j] < 0
-                then
+                if compare arr1.[i] arr2.[j] < 0 then
                     arr3.[k] <- arr1.[i]
                     i <- i + 1
                     k <- k + 1
@@ -23,13 +27,72 @@ module MergeSorting =
                     arr3.[k] <- arr2.[j]
                     j <- j + 1
                     k <- k + 1
-            if i >= Array.length arr1
-            then
-                for l in j..(Array.length arr2 - 1) do
+
+            if i >= Array.length arr1 then
+                for l in j .. (Array.length arr2 - 1) do
                     arr3.[k] <- arr2.[l]
                     k <- k + 1
             else
-                for l in i..(Array.length arr1 - 1) do
+                for l in i .. (Array.length arr1 - 1) do
                     arr3.[k] <- arr1.[l]
                     k <- k + 1
+
             arr3
+
+namespace BubbleLib
+
+module Bubblesorting =
+
+    let Bubblesort (arr1: 'a array) compare =
+        let mutable f: int = 1
+        let mutable i: int = 0
+
+        while (f = 1) do
+            let j: int = 0
+            f <- 0
+
+            for j in 0 .. Array.length arr1 - 2 - i do
+                if (compare arr1[j] arr1[j + 1] > 0) then
+                    let temp = arr1.[j]
+                    arr1.[j] <- arr1.[j + 1]
+                    arr1.[j + 1] <- temp
+                    f <- 1
+
+            i <- i + 1
+
+        arr1
+
+namespace QuickLib
+
+module Quicksorting =
+    let QuickSort (arr1: 'a array) compare =
+        let rec QuickHelper (arr: 'a array) (l: int) (r: int) =
+            let pivot = arr.[(l + r) / 2]
+            let mutable i: int = l
+            let mutable j: int = r
+
+            while i <= j do
+                while compare arr.[i] pivot < 0 do
+                    i <- i + 1
+
+                while compare arr.[j] pivot > 0 do
+                    j <- j - 1
+
+                if i <= j then
+                    let c = arr.[i]
+                    arr.[i] <- arr.[j]
+                    arr.[j] <- c
+                    i <- i + 1
+                    j <- j - 1
+
+            if l < j then
+                QuickHelper arr l j
+
+            if r > i then
+                QuickHelper arr i r
+
+        if arr1.Length > 0 then
+            QuickHelper arr1 0 (arr1.Length - 1)
+            arr1
+        else
+            [||]
--- tests/Tests.fs
+++ tests/Tests.fs
@@ -3,48 +3,143 @@ namespace tests
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
 open LibMergeSort
+open BubbleLib
+open QuickLib
 
 [<TestClass>]
-type TestClass () =
+type TestClass() =
 
     [<TestMethod>]
-    member this.TestMethodPassing1 () =
-        let expected = [|1; 2; 3; 4; 5; 6; 7|]
-        let actual = MergeSorting.MergeSort [|5; 1; 6; 4; 3; 7; 2|] compare
-        CollectionAssert.AreEqual(expected,  actual)
+    member this.TestMergeSort1() =
+        let expected = [| 1; 2; 3; 4; 5; 6; 7 |]
+        let actual = MergeSorting.MergeSort [| 5; 1; 6; 4; 3; 7; 2 |] compare
+        CollectionAssert.AreEqual(expected, actual)
 
     [<TestMethod>]
-    member this.TestMethodPassing2 () =
-        let expected = [|"abcd"; "abcde"; "abcdef"|]
-        let actual = MergeSorting.MergeSort [|"abcde"; "abcd"; "abcdef"|] compare
-        CollectionAssert.AreEqual(expected,  actual)
+    member this.TestMergeSort2() =
+        let expected = [| "abcd"; "abcde"; "abcdef" |]
+        let actual = MergeSorting.MergeSort [| "abcde"; "abcd"; "abcdef" |] compare
+        CollectionAssert.AreEqual(expected, actual)
 
     [<TestMethod>]
-    member this.TestMethodPassing3() =
+    member this.TestMergeSort3() =
         let expected = [| 1; 2; 3; 4; 5; 6; 7; 8 |]
-        let actual = MergeSorting.MergeSort[|3; 4; 1; 8; 5; 2; 7; 6 |] compare
+        let actual = MergeSorting.MergeSort [| 3; 4; 1; 8; 5; 2; 7; 6 |] compare
         CollectionAssert.AreEqual(expected, actual)
 
     [<TestMethod>]
-    member this.TestMethodPassing4() =
+    member this.TestMergeSort4() =
         let expected = [| -12.7; -4.4; 0; 4.2; 7; 10 |]
-        let actual:float array = MergeSorting.MergeSort [| 0; 10; -4.4; 4.2; 7; -12.7|] compare
+
+        let actual: float array =
+            MergeSorting.MergeSort [| 0; 10; -4.4; 4.2; 7; -12.7 |] compare
+
         CollectionAssert.AreEqual(expected, actual)
 
     [<TestMethod>]
-    member this.TestMethodPassing5() =
+    member this.TestMergeSort5() =
         let expected = [| 1 |]
         let actual = MergeSorting.MergeSort [| 1 |] compare
         CollectionAssert.AreEqual(expected, actual)
 
     [<TestMethod>]
-    member this.TestMethodPassing6() =
-        let expected = [| "a"; "b"; "d"; "f"; "g"; "y"; |]
-        let actual = MergeSorting.MergeSort [| "g"; "a"; "f"; "d"; "b"; "y"; |] compare
+    member this.TestMergeSort6() =
+        let expected = [| "a"; "b"; "d"; "f"; "g"; "y" |]
+        let actual = MergeSorting.MergeSort [| "g"; "a"; "f"; "d"; "b"; "y" |] compare
         CollectionAssert.AreEqual(expected, actual)
 
     [<TestMethod>]
-    member this.TestMethodPassing7() =
+    member this.TestMergeSort7() =
         let expected = [||]
         let actual = MergeSorting.MergeSort [||] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestBubbleSort1() =
+        let expected = [| 1; 2; 3; 4; 5; 6; 7 |]
+        let actual = Bubblesorting.Bubblesort [| 5; 1; 6; 4; 3; 7; 2 |] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestBubbleSort2() =
+        let expected = [| "abcd"; "abcde"; "abcdef" |]
+        let actual = Bubblesorting.Bubblesort [| "abcde"; "abcd"; "abcdef" |] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestBubbleSort3() =
+        let expected = [| 1; 2; 3; 4; 5; 6; 7; 8 |]
+        let actual = Bubblesorting.Bubblesort [| 3; 4; 1; 8; 5; 2; 7; 6 |] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestBubbleSort4() =
+        let expected = [| -12.7; -4.4; 0; 4.2; 7; 10 |]
+
+        let actual: float array =
+            Bubblesorting.Bubblesort [| 0; 10; -4.4; 4.2; 7; -12.7 |] compare
+
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestBubbleSort5() =
+        let expected = [| 1 |]
+        let actual = Bubblesorting.Bubblesort [| 1 |] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestBubbleSort6() =
+        let expected = [| "a"; "b"; "d"; "f"; "g"; "y" |]
+        let actual = Bubblesorting.Bubblesort [| "g"; "a"; "f"; "d"; "b"; "y" |] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestBubbleSort7() =
+        let expected = [||]
+        let actual = Bubblesorting.Bubblesort [||] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestQuickSort1() =
+        let expected = [| 1; 2; 3; 4; 5; 6; 7 |]
+        let actual = Quicksorting.QuickSort [| 5; 1; 6; 4; 3; 7; 2 |] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestQuickSort2() =
+        let expected = [| "abcd"; "abcde"; "abcdef" |]
+        let actual = Quicksorting.QuickSort [| "abcde"; "abcd"; "abcdef" |] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestQuickSort3() =
+        let expected = [| 1; 2; 3; 4; 5; 6; 7; 8 |]
+        let actual = Quicksorting.QuickSort [| 3; 4; 1; 8; 5; 2; 7; 6 |] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestQuickSort4() =
+        let expected = [| -12.7; -4.4; 0; 4.2; 7; 10 |]
+
+        let actual: float array =
+            Quicksorting.QuickSort [| 0; 10; -4.4; 4.2; 7; -12.7 |] compare
+
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestQuickSort5() =
+        let expected = [| 1 |]
+        let actual = Quicksorting.QuickSort [| 1 |] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestQuickSort6() =
+        let expected = [| "a"; "b"; "d"; "f"; "g"; "y" |]
+        let actual = Quicksorting.QuickSort [| "g"; "a"; "f"; "d"; "b"; "y" |] compare
+        CollectionAssert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestQuickSort7() =
+        let expected = [||]
+        let actual = Quicksorting.QuickSort [||] compare
         CollectionAssert.AreEqual(expected, actual)
