84f3f5a6f6abf6176e327ec77602cdcc9126133c
Grirory_Smirnov <smirnov2006.76@mail.ru>
2024-12-14 01:10:28 +0300

    Update code

---
+++ src/ImageProcessing/ImageProcessing.fs
@@ -0,0 +1,126 @@
+module ImageProcessing
+
+open System
+open SixLabors.ImageSharp
+open SixLabors.ImageSharp.PixelFormats
+
+type Rgb = { r: byte; g: byte; b: byte }
+
+[<Struct>]
+type Image =
+    val Data: array<Rgb>
+    val Width: int
+    val Height: int
+    val Name: string
+
+    new(data, width, height, name) =
+        { Data = data
+          Width = width
+          Height = height
+          Name = name }
+
+let loadAs2DArray (file: string) =
+    let img = Image.Load<Rgb24> file
+
+    let res =
+        Array2D.init img.Height img.Width (fun j i ->
+            let pixel = img.[i, j]
+
+            { r = pixel.R
+              g = pixel.G
+              b = pixel.B })
+
+    printfn $"H=%A{img.Height} W=%A{img.Width}"
+    printfn $"%A{img.Metadata}"
+    res
+
+(*let loadAsImage (file: string) =
+    let img = Image.Load<Rgb24> file
+
+    let buf = Array.init (img.Width * img.Height) (fun _ -> { r = 0uy; g = 0uy; b = 0uy; })
+
+    img.CopyPixelDataTo(Span<Rgb24> buf)
+    Image(buf, img.Width, img.Height, System.IO.Path.GetFileName file)*)
+
+let save2DByteArrayAsImage (imageData: Rgb[,]) file =
+    let h = imageData.GetLength 0
+    let w = imageData.GetLength 1
+    printfn $"H=%A{h} W=%A{w}"
+
+    let flat2Darray array2D =
+        [| for x in [ 0 .. (Array2D.length1 array2D) - 1 ] do
+               for y in [ 0 .. (Array2D.length2 array2D) - 1 ] do
+                   yield array2D.[x, y] |]
+
+     // Преобразование Rgb в Rgb24
+    let flatRgb24Array = 
+        flat2Darray imageData 
+        |> Array.map (fun rgb -> Rgb24(rgb.r, rgb.g, rgb.b))
+
+    // Создание изображения из одномерного массива
+    let img = Image.LoadPixelData<Rgb24>(flatRgb24Array, w, h)
+    img.Save(file)
+
+(*let saveImage (image: Image) file =
+    let img = Image.LoadPixelData<Rgb24>(image.Data, image.Width, image.Height)
+    img.Save file*)
+
+let gaussianBlurKernel =
+    [| [| 0.000789; 0.006581; 0.013347; 0.006581; 0.000789 |]
+       [| 0.006581; 0.054901; 0.111345; 0.054901; 0.006581 |]
+       [| 0.013347; 0.111345; 0.225821; 0.111345; 0.013347 |]
+       [| 0.006581; 0.054901; 0.111345; 0.054901; 0.006581 |]
+       [| 0.000789; 0.006581; 0.013347; 0.006581; 0.000789 |] |]
+    |> Array.map (Array.map (fun x -> (float32 x) / 256.0f))
+
+let edgesKernel =
+    [| [| 0; 0; -1; 0; 0 |]
+       [| 0; 0; -1; 0; 0 |]
+       [| 0; 0; 2; 0; 0 |]
+       [| 0; 0; 0; 0; 0 |]
+       [| 0; 0; 0; 0; 0 |] |]
+    |> Array.map (Array.map (fun x -> (float32 x) / 256.0f))
+
+let sharpnessKernel =
+    [| [| 0; 0; 0; 0; 0 |]
+       [| 0; 0; -1; 0; 0 |]
+       [| 0; -1; 5; -1; 0 |]
+       [| 0; 0; -1; 0; 0 |]
+       [| 0; 0; 0; 0; 0 |] |]
+    |> Array.map (Array.map (fun x -> (float32 x) / 256.0f))
+
+let applyFilter (filter: float32[][]) (img: Rgb[,]) =
+    let imgH = img.GetLength 0
+    let imgW = img.GetLength 1
+
+    let filterD = (Array.length filter) / 2
+
+    let filter = Array.concat filter
+
+    let processPixel px py =
+        let mutable sumR = 0.0f
+        let mutable sumG = 0.0f
+        let mutable sumB = 0.0f
+
+        for i in px - filterD .. px + filterD do
+            for j in py - filterD .. py + filterD do
+                if i >= 0 && i < imgH && j >= 0 && j < imgW then
+                    let pixel = img.[i, j]
+                    let value = filter.[i + filterD].[j + filterD]
+                    sumR <- sumR + (float32 pixel.r * value)
+                    sumG <- sumG + (float32 pixel.g * value)
+                    sumB <- sumB + (float32 pixel.b * value)
+                else 
+                    let pixel = img.[px, py]
+                    let value = filter.[px + filterD].[py + filterD]
+                    sumR <- sumR + (float32 pixel.r * value)
+                    sumG <- sumG + (float32 pixel.g * value)
+                    sumB <- sumB + (float32 pixel.b * value)
+
+        // Ограничиваем значения RGB в диапазоне [0, 255]
+        { r = byte (min 255 (max 0 (int sumR)))
+          g = byte (min 255 (max 0 (int sumG)))
+          b = byte (min 255 (max 0 (int sumB))) }
+
+    Array2D.mapi (fun x y _ -> (processPixel x y)) img
+    
--- src/ImageProcessing/Program.fs
+++ src/ImageProcessing/Program.fs
@@ -1,94 +1,39 @@
-﻿open System
-open System.Drawing
-open Microsoft.FSharp.Collections
+﻿open ImageProcessing
 open Argu
 
-type CliArguments =
-    | Working_Directory of path: string
-    | Listener of host: string * port: int
-    | Data of base64: byte[]
-    | Port of tcp_port: int
-    | Log_Level of level: int
-    | Detach
+let pathToExamples = "/home/gregory/demo_2024/FsharpProj/src/ImageProcessing/pics/"
+let inputFolder = System.IO.Path.Combine(pathToExamples, "input")
+
+let demoFile =
+    System.IO.Path.Combine(inputFolder, "armin-djuhic-ohc29QXbS-s-unsplash.jpg")
+
+type CmdArgs =
+    | [<Mandatory>] Input_File of string
+    | [<Mandatory>] Out_File of string
 
     interface IArgParserTemplate with
-        member s.Usage =
-            match s with
-            | Working_Directory _ -> "specify a working directory."
-            | Listener _ -> "specify a listener (hostname : port)."
-            | Data _ -> "binary data in base64 encoding."
-            | Port _ -> "specify a primary port."
-            | Log_Level _ -> "set the log level."
-            | Detach -> "detach daemon from console."
+        member this.Usage =
+            match this with
+            | Input_File _ -> "File to process."
+            | Out_File _ -> "Where to save result."
 
-module ImageProcessing =
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+    let parser = ArgumentParser.Create<CmdArgs>(programName = "ImageProcessing")
+    let usage = parser.PrintUsage()
+    let results = parser.Parse argv
+    let inFile = results.GetResult Input_File
+    let outFile = results.GetResult Out_File
+    let inImage = loadAs2DArray inFile
 
-    let filter1 = array2D [ [ 0.0; 0.0; 0.0 ]; [ 0.0; 1.0; 0.0 ]; [ 0.0; 0.0; 0.0 ] ]
-
-    let coef = 1.0
-    let bias = 0.0
-    type ColorRGB = { R: byte; G: byte; B: byte }
-
-    let LoadImage(* (filename: string)*) : ColorRGB array2d =
-
-        use bitmap = new Bitmap("src/ImageProcessing/pics/flower.png", true)
-        let width = bitmap.Width
-        let height = bitmap.Height // Исправлено на Height
-        let image = Array2D.init height width (fun i j -> { R = 0uy; G = 0uy; B = 0uy }) // Исправлено на Color.FromArgb
-
-        for y in 0 .. height - 1 do
-            for x in 0 .. width - 1 do
-                let color = bitmap.GetPixel(x, y)
-
-                image[y, x] <-
-                    { R = color.R
-                      G = color.G
-                      B = color.B }
-
-        image // Исправлено на height
-
-    let ApplieFilter (filter: float array2d) (image: ColorRGB array2d) =
-
-        let width = Array2D.length2 image
-        let height = Array2D.length1 image
-
-        for y in 0 .. height - 1 do
-            for x in 0 .. width - 1 do
-                let mutable red, green, blue = 0.0, 0.0, 0.0
-
-                for fy in 0..2 do
-                    for fx in 0..2 do
-                        let imageX = (x - 1 + fx + width) % width
-                        let imageY = (y - 1 + fy + height) % height
-                        red <- red + float image[imageY, imageX].R * filter[fy, fx]
-                        green <- green + float image[imageY, imageX].G * filter[fy, fx]
-                        blue <- blue + float image[imageY, imageX].B * filter[fy, fx]
-
-                image[y, x] <-
-                    { R = byte (Math.Min(Math.Max(int (coef * red + bias), 0), 255))
-                      G = byte (Math.Min(Math.Max(int (coef * green + bias), 0), 255))
-                      B = byte (Math.Min(Math.Max(int (coef * blue + bias), 0), 255)) }
-
-        image
-
-    let CreateImage (image: ColorRGB array2d) (filename: string) =
-
-        let width = Array2D.length2 image
-        let height = Array2D.length1 image
-        use bitmap = new Bitmap(width, height)
-
-        for y in 0 .. height - 1 do
-            for x in 0 .. width - 1 do
-                let color = Color.FromArgb(int image[y, x].R, int image[y, x].G, int image[y, x].B)
-                bitmap.SetPixel(x, y, color)
-
-        bitmap.Save(filename)
-
-
-    [<EntryPoint>]
-    let main args =
-        // Здесь можно вызвать LoadImage или выполнить другие действия
-        let mutable image = LoadImage
-        image <- ApplieFilter filter1 image
-        CreateImage image args.[1]
-        0
+    let resultImage =
+        //applyFilter sharpnessKernel inImage
+        //applyFilter edgesKernel inImage
+        applyFilter gaussianBlurKernel inImage
+    //|> applyFilter gaussianBlurKernel
+    //|> applyFilter gaussianBlurKernel
+    //|> applyFilter gaussianBlurKernel
+    //|> applyFilter edgesKernel
+    save2DByteArrayAsImage resultImage outFile
+    0
