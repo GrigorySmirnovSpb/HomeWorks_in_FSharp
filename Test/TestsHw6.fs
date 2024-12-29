ebc24b9bcb951dd772b85ff06af5eea25e375752
Grirory_Smirnov <smirnov2006.76@mail.ru>
2024-12-29 16:27:47 +0300

    Update code

--- Test/TestsHw6.fs
+++ Test/TestsHw6.fs
@@ -10,51 +10,43 @@ open TreeLib
 
 module PropertyTests =
 
-    let areAlmostEqual (a: float32) (b: float32) (epsilon: float32) =
-        if Single.IsFinite(a) && Single.IsFinite(b) then
-            abs (a - b) < epsilon
-        elif Single.IsNaN(a) && Single.IsNaN(b) then
-            true
-        elif Single.IsInfinity(a) && Single.IsInfinity(b) then
-            true
-        elif Single.IsNegativeInfinity(a) && Single.IsNegativeInfinity(b) then
-            true
-        elif Single.IsPositiveInfinity(a) && Single.IsPositiveInfinity(b) then
-            true
-        else
-            false
     type heightTest() =
 
         [<Fact>]
-        let heightTest() =
+        let heightTest () =
             let leaf1 = MyTree.createLeaf 1
             let leaf2 = MyTree.createLeaf 2
             let leaf3 = MyTree.createLeaf 3
 
-            let tree1 = MyTree.createTree (NonEmptyList.ofList [leaf2])
-            let node1 = MyTree.createTree (NonEmptyList.ofList [leaf1; tree1])
-            let node2 = MyTree.createTree (NonEmptyList.ofList [leaf3])
+            let tree1 = MyTree.createTree (NonEmptyList.ofList [ leaf2 ])
+            let node1 = MyTree.createTree (NonEmptyList.ofList [ leaf1; tree1 ])
+            let node2 = MyTree.createTree (NonEmptyList.ofList [ leaf3 ])
 
-            let tree = MyTree.createTree (NonEmptyList.ofList [node1; node2])
+            let tree = MyTree.createTree (NonEmptyList.ofList [ node1; node2 ])
             Assert.Equal(3, MyTree.height tree)
-        
+
     [<Properties(MaxTest = 100)>]
     type idTests() =
 
         [<Property>]
-        member _.intTest(tree: MyTree<int>) = tree = MyTree.map id tree
+        member _.intTest(tree: MyTree<int>) =
+            let treem = MyTree.map id tree
+            Assert.Equal(tree, treem)
 
         [<Property>]
-        member _.charTest(tree: MyTree<char>) = tree = MyTree.map id tree
+        member _.charTest(tree: MyTree<char>) =
+            let treem = MyTree.map id tree
+            Assert.Equal(tree, treem)
 
         [<Property>]
-        member _.floatTest(tree: MyTree<float32>) =
-            let treeList = MyTree.treeToList (MyTree.map id tree)
-            let list = MyTree.treeToList tree
-            Assert.True(List.forall2 (fun a b -> areAlmostEqual a b 1e-10f) list treeList)
+        member _.floatTest(tree: MyTree<float>) =
+            let treem = MyTree.map id tree
+            Assert.Equal(tree, treem)
 
         [<Property>]
-        member _.arrTest(tree: MyTree<array<int>>) = tree = MyTree.map id tree
+        member _.arrTest(tree: MyTree<array<int>>) =
+            let treem = MyTree.map id tree
+            Assert.Equal(tree, treem)
 
     type sumTests() =
 
@@ -83,8 +75,7 @@ module PropertyTests =
         member _.floatFoldTest(tree: MyTree<float32>) =
             let sum acc value = value + acc
             let actsum = MyTree.fold sum 0f tree
-            let expsum = List.fold (fun x acc -> sum acc x) 0f (MyTree.treeToList tree) 
-            //Assert.True(areAlmostEqual actsum expsum 1e-10f)
+            let expsum = List.fold (fun x acc -> sum acc x) 0f (MyTree.treeToList tree)
             Assert.Equal(actsum, expsum)
 
         [<Property>]
@@ -120,13 +111,13 @@ module PropertyTests =
         [<Property>]
         member _.intFoldTest(tree: MyTree<int>) =
             let prod acc value = value * acc
-            let actprod = MyTree.fold prod 1 tree
-            let expprod = List.fold prod 1 (MyTree.treeToList tree)
+            let actprod = MyTree.fold prod 2 tree
+            let expprod = List.fold prod 2 (MyTree.treeToList tree)
             Assert.Equal(actprod, expprod)
 
         [<Property>]
         member _.floatFoldTest(tree: MyTree<float32>) =
             let prod acc value = value * acc
-            let actprod = MyTree.fold prod 1f tree
-            let expprod = MyTree.treeToList tree |> List.fold prod 1f
-            Assert.True(areAlmostEqual actprod expprod 1e-10f)
+            let actprod = MyTree.fold prod 2f tree
+            let expprod = List.fold prod 2f (MyTree.treeToList tree)
+            Assert.Equal(actprod, expprod)
