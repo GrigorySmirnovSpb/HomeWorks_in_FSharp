b9e20bdd1536ff4425d3d70279ad9f1097f8edca
Grirory_Smirnov <smirnov2006.76@mail.ru>
2025-03-03 23:05:37 +0300

    Add new tests

--- Test/TestsHw6.fs
+++ Test/TestsHw6.fs
@@ -8,7 +8,7 @@ open FsCheck.Xunit
 open FSharpPlus.Data
 open TreeLib
 
-module PropertyTests =
+module PropertyTreeTests =
 
     type heightTest() =
 
@@ -47,7 +47,7 @@ module PropertyTests =
             Assert.Equal(tree, treem)
 
         [<Property>]
-        member _.floatTest(tree: MyTree<float>) =
+        member _.floatTest(tree: MyTree<float32>) =
             let treem = MyTree.map id tree
             Assert.Equal(tree, treem)
 
@@ -56,13 +56,39 @@ module PropertyTests =
             let treem = MyTree.map id tree
             Assert.Equal(tree, treem)
 
+    type MapTests() =
+
+        [<Property>]
+        member _.intTest(tree: MyTree<int>) =
+            let treem = MyTree.treeToList (MyTree.map ((+) 2) tree)
+            let expList = List.map ((+) 2) (MyTree.treeToList tree)
+            expList = treem
+
+        [<Property>]
+        member _.charTest(tree: MyTree<char>) =
+            let treem = MyTree.treeToList (MyTree.map (fun c -> char (int c + 2)) tree)
+            let expList = List.map (fun c -> char (int c + 2)) (MyTree.treeToList tree)
+            expList = treem
+
+        [<Property>]
+        member _.floatTest(tree: MyTree<float32>) =
+            let treem = MyTree.treeToList (MyTree.map ((+) 2f) tree)
+            let expList = List.map ((+) 2f) (MyTree.treeToList tree)
+            Assert.True(List.forall2 (fun a b -> areAlmostEqual a b 1e-10f)expList treem)
+
+        [<Property>]
+        member _.arrTest(tree: MyTree<array<int>>) =
+            let treem = MyTree.treeToList (MyTree.map (Array.map ((+) 2)) tree)
+            let expList = List.map (Array.map ((+) 2)) (MyTree.treeToList tree)
+            expList = treem
+
     type sumTests() =
 
         [<Property>]
-        member _.intFoldTest(tree1, tree2, tree3: MyTree<int>) =
+        member _.intFoldTest(tree: MyTree<int>) =
             let sum acc value = value + acc
-            let actsum = MyTree.fold sum (MyTree.fold sum (MyTree.fold sum 0 tree1) tree2) tree3
-            let expsum = MyTree.fold sum (MyTree.fold sum (MyTree.fold sum 0 tree2) tree3) tree1
+            let actsum = MyTree.fold sum 0 tree
+            let expsum = int (List.sum (MyTree.treeToList tree))
             Assert.Equal(actsum, expsum)
 
         [<Property>]
@@ -130,7 +156,7 @@ module PropertyTests =
             let expprod = List.fold prod 2f (MyTree.treeToList tree)
             Assert.Equal(actprod, expprod)
 
-    type AssocSumTests() =
+    type AssocSumFoldsTests() =
 
         [<Property>]
         member _.intFoldTest(tree1, tree2, tree3: MyTree<int>) =
@@ -173,3 +199,25 @@ module PropertyTests =
             let actsum = MyTree.foldBack sum (MyTree.foldBack sum (MyTree.foldBack sum 0 tree1) tree2) tree3
             let expsum = MyTree.foldBack sum (MyTree.foldBack sum (MyTree.foldBack sum 0 tree2) tree3) tree1
             Assert.Equal(actsum, expsum)
+
+
+
+    type AssocMapTests() =
+
+        [<Property>]
+        member _.intTest(tree: MyTree<int>) =
+            let treem = MyTree.map ((+) -4) tree |> MyTree.map ((+) 3) |> MyTree.map ((+) 2)
+            let treem2 = MyTree.map ((+) 2) tree |> MyTree.map ((+) -4) |> MyTree.map ((+) 3)
+            Assert.Equal(treem, treem2)
+
+        [<Property>]    
+        member _.charTest(tree: MyTree<char>) =
+            let treem = MyTree.map (fun c -> char (int c + -4)) tree |> MyTree.map (fun c -> char (int c + 3)) |> MyTree.map (fun c -> char (int c + 2))
+            let treem2 = MyTree.map (fun c -> char (int c + 2)) tree |> MyTree.map (fun c -> char (int c + -4)) |> MyTree.map (fun c -> char (int c + 3))
+            Assert.Equal(treem, treem2)
+
+        [<Property>]
+        member _.arrTest(tree: MyTree<array<int>>) =
+            let treem = MyTree.map (Array.map ((+) -4)) tree |> MyTree.map (Array.map ((+) 3)) |> MyTree.map (Array.map ((+) 2))
+            let treem2 = MyTree.map (Array.map ((+) 2)) tree |> MyTree.map (Array.map ((+) -4)) |> MyTree.map (Array.map ((+) 3))
+            Assert.Equal(treem, treem2)
